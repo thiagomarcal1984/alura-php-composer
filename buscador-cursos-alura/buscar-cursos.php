f0dc89916dd19301bbbc4bb7f268b03ecdc5b3e6
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-07-02 14:15:42 -0300

    Buscando os cursos da Alura.

--- buscador-cursos-alura/buscar-cursos.php
+++ buscador-cursos-alura/buscar-cursos.php
@@ -1,8 +1,25 @@
 <?php
 
-use GuzzleHttp\Client;
+require 'vendor/autoload.php';
 
-$client = new Client();
+use \GuzzleHttp\Client;
+use \Symfony\Component\DomCrawler\Crawler;
+
+
+$client = new Client(['verify' => false]);
 $resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-tecnologia');
+// O parâmetro verify informa se o certificado SSH será verificado.
+// Outra alternativa para o código de cima é colocar o array no método request:
+// $client = new Client();
+// $resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-tecnologia', ['verify' => false]);
 
 $html = $resposta->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter('span.card-curso__nome');
+
+foreach($cursos as $curso) {
+    echo $curso->textContent . PHP_EOL;
+}
